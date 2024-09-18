f754d69d0d382257aa95f1716d8a2bbbea9a5351
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-09-18 21:21:18 +0300

    feat: add carousel img

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export const data_img = [
   { srcDesc: "https://uk.triumph.com/dw/image/v2/AAFY_PRD/on/demandware.static/-/Library-Sites-Triumph/default/dw20658538/redesign/PageDesigner/Homepages/SALE_NowOrNever/Triumph_BANNERS_NowOrNever_Desktop_1148x448px_GB.jpg?sh=645&q=70",
     scrMob: 'https://uk.triumph.com/dw/image/v2/AAFY_PRD/on/demandware.static/-/Library-Sites-Triumph/default/dwfa7590bc/redesign/PageDesigner/Homepages/GET20/TRIUMPH_BANNERS_AMOURETTE_300_PROMO_Mobile_414x355px_GB.jpg?sh=836&q=70' },
   { srcDec: "https://uk.triumph.com/dw/image/v2/AAFY_PRD/on/demandware.static/-/Library-Sites-Triumph/default/dw1fb8e2a0/redesign/PageDesigner/Homepages/MemberDays/TRIUMPH_BANNERS_MEMBERDAYS_Desktop_1148x448px_GB.jpg?sh=645&q=70", 
-    scrMob: 'https://uk.triumph.com/dw/image/v2/AAFY_PRD/on/demandware.static/-/Library-Sites-Triumph/default/dwfa7590bc/redesign/PageDesigner/Homepages/GET20/TRIUMPH_BANNERS_AMOURETTE_300_PROMO_Mobile_414x355px_GB.jpg?sh=836&q=70' },
+    scrMob: 'https://uk.triumph.com/dw/image/v2/AAFY_PRD/on/demandware.static/-/Library-Sites-Triumph/default/dw20658538/redesign/PageDesigner/Homepages/SALE_NowOrNever/Triumph_BANNERS_NowOrNever_Desktop_1148x448px_GB.jpg?sh=645&q=70' },
   { srcDec: "https://uk.triumph.com/dw/image/v2/AAFY_PRD/on/demandware.static/-/Library-Sites-Triumph/default/dwcef0be8a/Triumph_Homepage_Lift_D.jpg?sh=645&q=70", 
     scrMob: 'https://uk.triumph.com/dw/image/v2/AAFY_PRD/on/demandware.static/-/Library-Sites-Triumph/default/dw56ed21b3/redesign/PageDesigner/Homepages/GET20/TRIUMPH_BANNERS_AMOURETTE_300_PROMO_Desktop_1148x448px_GB.jpg?sh=645&q=70' },
   { srcDec: "https://uk.triumph.com/dw/image/v2/AAFY_PRD/on/demandware.static/-/Library-Sites-Triumph/en_GB/dw083e0bdd/SliderOfSmartThermal_Desktop_GB.jpg?sh=645&q=70",
--- index.js
+++ index.js
@@ -1,74 +1,203 @@
 import { data_img, data_text } from './data.js'
 
 
-function createCarouselElements(){
-  data_text.forEach((elem,i)=>{
+function createCarouselElementsText() {
+  data_text.forEach((elem, i) => {
     const item = document.createElement("div");
     item.classList.add("carousel__content");
-    if(i==0){
+    if (i == 0) {
       item.classList.add("active");
     }
-    item.innerHTML =`
+    item.innerHTML = `
             <div class="carousel-item">${elem.text}
               <a href="/" class="carousel-item-link">${elem.link}</a>
             </div>`;
     document.querySelector(".carousel-inner").appendChild(item)
   })
 }
-createCarouselElements();
+createCarouselElementsText();
+const itemsImg = document.querySelectorAll('.carousel__item')
 
-let items = document.querySelectorAll('.carousel__content');
-let currentItem = 0;
-let isEnabled = true;
+function createCarouselElementsImg() {
+  itemsImg.forEach((elem, i) => {
+    console.log(i);
+    elem.setAttribute('data-img', i)
+    elem.style.backgroundImage = `url(${data_img[i].scrMob})`;
+  })
+}
+function createCarouselElementsImgButton() {
+  data_img.forEach((_, i) => {
+    const item = document.createElement("button");
+    item.classList.add("button-indicator");
+    if (i == 0) {
+      item.classList.add("active");
+    }
+    item.setAttribute('data-active', i)
+    document.querySelector(".carousel__indicators").appendChild(item)
+  })
+}
+createCarouselElementsImgButton()
+createCarouselElementsImg();
+const buttonActive = document.querySelectorAll('.button-indicator')
+function removeActive(items){
+  items.forEach((el)=>{
+el.classList.remove('active')
+  })
+}
+// console.log(buttonActive);
 
-function changeCurrentItems(n){
-currentItem = (n + items.length) % items.length;
-console.log(currentItem);
+let itemsText = document.querySelectorAll('.carousel__content');
+let currentItemText = 0;
+let isEnabledText = true;
+
+function changeCurrentItems(n) {
+  currentItemText = (n + itemsText.length) % itemsText.length;
 }
 
-function hideItem(direction){
-console.log(items[currentItem]);
-    isEnabled = false;
-    items[currentItem].classList.add(direction)
-    items[currentItem].addEventListener('animationend',function(){
-        this.classList.remove('active', direction);
-    })
+function hideItem(direction) {
+  isEnabledText = false;
+  itemsText[currentItemText].classList.add(direction)
+  itemsText[currentItemText].addEventListener('animationend', function () {
+    this.classList.remove('active', direction);
+  })
 }
-function showItem(direction){
-console.log(items[currentItem]);
-    items[currentItem].classList.add('next', direction)
-    items[currentItem].addEventListener('animationend',function(){
-        this.classList.remove('next', direction);
-        this.classList.add('active')
-        isEnabled = true
-    })
+function showItem(direction) {
+  itemsText[currentItemText].classList.add('next', direction)
+  itemsText[currentItemText].addEventListener('animationend', function () {
+    this.classList.remove('next', direction);
+    this.classList.add('active')
+    isEnabledText = true
+  })
 }
 
-function prevItem (n){
-    hideItem('to-right')
-    changeCurrentItems(n-1)
-    showItem('from-left')
+function prevItem(n) {
+  hideItem('to-right')
+  changeCurrentItems(n - 1)
+  showItem('from-left')
 }
-function nextItem (n){
-    hideItem('to-left')
-    changeCurrentItems(n+1)
-    showItem('from-right')
+function nextItem(n) {
+  hideItem('to-left')
+  changeCurrentItems(n + 1)
+  showItem('from-right')
 }
 
 
-document.querySelector('.carousel-control-prev').addEventListener('click',function(){
-  if(isEnabled){
-    prevItem(currentItem)
+document.querySelector('.carousel-control-prev').addEventListener('click', function () {
+  if (isEnabledText) {
+    prevItem(currentItemText)
   }
 })
-document.querySelector('.carousel-control-next').addEventListener('click',function(){
+document.querySelector('.carousel-control-next').addEventListener('click', function () {
   clearInterval(setTime)
-  if(isEnabled){
-    nextItem(currentItem)
+  if (isEnabledText) {
+    nextItem(currentItemText)
   }
 })
-const setTime = setInterval(()=>{
-  if(isEnabled){
-    nextItem(currentItem)
+const setTime = setInterval(() => {
+  if (isEnabledText) {
+    nextItem(currentItemText)
   }
-},3000)
+}, 3000)
+
+const BTN_RIGHT = document.querySelector('.button--next');
+const BTN_LEFT = document.querySelector('.button--prev');
+const CAROUSEL = document.querySelector('.carousel__container')
+// //движение карусели
+const move_left = () => {
+  CAROUSEL.classList.add('transition_left')
+  //на время анимации отключить кнопки для этого создаём функцию 
+  BTN_RIGHT.removeEventListener('click', move_right);
+  BTN_LEFT.removeEventListener('click', move_left)
+}
+const move_right = () => {
+  CAROUSEL.classList.add('transition_right')
+  //на время анимации отключить кнопки для этого содаём функцию 
+  BTN_LEFT.removeEventListener('click', move_left)
+  BTN_RIGHT.removeEventListener('click', move_right)
+}
+
+BTN_LEFT.addEventListener('click', move_left)
+BTN_RIGHT.addEventListener('click', move_right)
+
+let currentItemImg = 0;
+let AllItemImg = data_img.length;
+// //слушаем конец анимации
+CAROUSEL.addEventListener('animationend', (animationEvent) => {//добавляем в функцию аргумент событие
+
+  let changedItem; //переменная меняется в зависимости от направления
+  if (animationEvent.animationName === 'move_left') {
+    CAROUSEL.classList.remove('transition_left')
+    const ITEM_LEFT = document.querySelector('#item_left')
+    changedItem = ITEM_LEFT.getAttribute('data-img')
+    let leftItem;
+    let rightItem;
+    // to main section
+    const ITEM_ACTIVE = document.querySelector('#item_active')
+    ITEM_ACTIVE.style.backgroundImage = `url(${data_img[changedItem].scrMob})`;
+    ITEM_ACTIVE.setAttribute('data-img', changedItem)
+    removeActive(buttonActive)
+    document.querySelector(`[data-active="${changedItem}"]`).classList.add('active')
+    currentItemImg = changedItem;
+    // to right section
+    if (+changedItem + 1 > AllItemImg - 1) {
+      rightItem = 0
+    } else {
+      rightItem = +changedItem + 1
+    }
+    const ITEM_RIGHT = document.querySelector('#item_right')
+    ITEM_RIGHT.style.backgroundImage = `url(${data_img[rightItem].scrMob})`;
+    ITEM_RIGHT.setAttribute('data-img', rightItem)
+    // to left section
+    changedItem--
+    if (changedItem < 0) {
+      leftItem = AllItemImg - 1
+    } else {
+      leftItem = changedItem
+    }
+    ITEM_LEFT.style.backgroundImage = `url(${data_img[leftItem].scrMob})`
+    ITEM_LEFT.setAttribute('data-img', leftItem)
+
+  } else {
+    CAROUSEL.classList.remove('transition_right')
+    const ITEM_RIGHT = document.querySelector('#item_right')
+    changedItem = ITEM_RIGHT.getAttribute('data-img')
+
+    const ITEM_LEFT = document.querySelector('#item_left')
+    let leftItem;
+    let rightItem;
+    
+    // to main section
+    const ITEM_ACTIVE = document.querySelector('#item_active')
+    ITEM_ACTIVE.style.backgroundImage = `url(${data_img[changedItem].scrMob})`;
+    ITEM_ACTIVE.setAttribute('data-img', changedItem)
+    currentItemImg = changedItem;
+
+
+    if (changedItem-1 < 0) {
+      leftItem = AllItemImg - 1
+    } else {
+      leftItem = changedItem -1
+    }
+
+    ITEM_LEFT.style.backgroundImage = `url(${data_img[leftItem].scrMob})`
+    ITEM_LEFT.setAttribute('data-img', leftItem)
+    // to right section
+    changedItem++
+    if (+changedItem > AllItemImg - 1) {
+      rightItem = 0;
+    } else {
+      rightItem = +changedItem;
+    }
+    // const ITEM_RIGHT = document.querySelector('#item_right')
+    ITEM_RIGHT.style.backgroundImage = `url(${data_img[rightItem].scrMob})`;
+    ITEM_RIGHT.setAttribute('data-img', rightItem)
+    // to left section
+    // changedItem--
+    
+
+  }
+
+  //возвращаем слушание события для кнопок
+  BTN_RIGHT.addEventListener('click', move_right)
+  BTN_LEFT.addEventListener('click', move_left)
+})
